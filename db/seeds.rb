a212ae47367fdd4d8ca574e55450954a1d77d860
ericscottmarquez <ericscottmarquez@gmail.com>
2018-06-29 19:40:15 -0600

    created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,6 +25,8 @@
     )
 end
 
+puts "10 blog posts created"
+
 5.times do |skill|
   Skill.create!(
     title: "Rails #{skill}",
@@ -33,6 +35,35 @@ end
 end
 
 puts "5 skills created"
+
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "My great service",
+    body: "But I must explain to you how all this mistaken idea of 
+    enouncing pleasure and praising pain was born and I will
+     give you a complete account of the system, and expound 
+     the actual teachings of the great explorer of the truth,
+      the master-builder of human happiness. No one rejects, 
+      dislikes, or avoids pleasure itself, because it is 
+      pleasure, but because those who do not know how to 
+        pursue pleasure rationally encounter consequences 
+        that are extremely painful. Nor again is there anyone 
+        who loves or pursues or desires to obtain pain of itself, 
+        because it is pain, but because occasionally circumstances 
+        occur in which toil and pain can procure him some great 
+        pleasure. To take a trivial example, which of us ever
+         undertakes laborious physical exercise, except to obtain
+          some advantage from it? But who has any right to find 
+          fault with a man who chooses to enjoy a pleasure that 
+          has no annoying consequences, or one who avoids a pain
+           that produces no resultant pleasure?",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200"
+    )
+end
+
 
 puts "9 portfolio items created"
 
