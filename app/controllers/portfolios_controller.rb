93e1e14d48a187f8ffd8498b1e5618602535d261
ericscottmarquez <ericscottmarquez@gmail.com>
2018-07-05 18:21:18 -0600

    added small destroy action for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -30,7 +30,6 @@ class PortfoliosController < ApplicationController
   def update
     @portfolio_item = Portfolio.find(params[:id])
 
-
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'record was successfully updated.' }
@@ -45,6 +44,18 @@ class PortfoliosController < ApplicationController
   def show
     @portfolio_item = Portfolio.find(params[:id])
   end
+
+  def destroy
+    # perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+    # destroy the record
+    @portfolio_item.destroy
+    #redirect to the path and get out of there
+    respond_to do |format|
+    format.html { redirect_to portfolios_path, notice: 'record was successfully destroyed.' }
+    end
+  end
+
 
 
 end
