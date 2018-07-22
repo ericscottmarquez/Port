763e37ed344ebf7c13fe6b6e7581b7431ecd9e12
ericscottmarquez <ericscottmarquez@gmail.com>
2018-07-21 19:50:41 -0600

    completed authentication on user model for virtual attributes for name and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_presence_of :first_name
+
+  def first_name
+    self.name.split.first
+
+  end
+
+  def last_name
+    self.name.split.last
+
+
+  end
 
 
 
