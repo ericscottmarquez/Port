50ce066f1553c5a1a9e1e445afa44393c6cb02ab
ericscottmarquez <ericscottmarquez@gmail.com>
2018-08-08 18:05:44 -0600

    implemented partials for collections on the portfolios page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,10 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
+
+Rails.application.config.assets.precompile += %w( blogs.css, portfolios.scss, blogs.scss, portfolios.scss )
+Rails.application.config.assets.precompile += %w( blogs.css )
+
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
