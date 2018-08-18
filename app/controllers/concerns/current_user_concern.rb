d9b6725ccfe1e6dbec42e97ed78f5cb12f33c232
ericscottmarquez <ericscottmarquez@gmail.com>
2018-08-17 20:14:07 -0600

    added roles for admin user

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -11,11 +11,12 @@ module CurrentUserConcern
 
 
   def guest_user
-    OpenStruct.new(name: "Guest User",
-                  first_name: "Guest",
-                  last_name: "User",
-                  email: "email@example.com"
-                  )
+    guest = GuestUser.new
+    guest.name = "Gues User"
+    guest.first_name = "Guest"
+    guest.last_name = "User"
+    guest.email = "guest@example.com"
+    guest
   end
 
 
