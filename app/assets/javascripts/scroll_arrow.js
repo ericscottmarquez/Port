469ed3b36659d2bbbbd8a1f4eca188d8d073e017
ericscottmarquez <ericscottmarquez@gmail.com>
2018-09-07 02:25:32 -0600

    added scrolly arrow and made particlesjs nicer and static on the page

---
+++ app/assets/javascripts/scroll_arrow.js
@@ -0,0 +1,17 @@
+
+
+
+$(window).scroll(function() {
+
+    if ($(this).scrollTop()>0)
+     {
+        $('.d').fadeOut(500);
+     }
+    else
+     {
+      $('.d').fadeIn(500);
+     }
+
+ });
+
+   
