7e118d11782beaaee1fc225d75b5190d507fc2c7
ericscottmarquez <ericscottmarquez@gmail.com>
2018-09-28 02:50:12 -0600

    changed precompiler in application.rb for heroku since it failed push for precompiling assets

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module Drc
     # the framework and any gems in your application.
     # Don't generate system test files.
     config.generators.system_tests = nil
-
+    config.assets.initialize_on_precompile = false
+    
   end
 end
