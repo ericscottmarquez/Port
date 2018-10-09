92a897c92c3c3f727aea365ca17dbb976e6f8d7c
ericscottmarquez <ericscottmarquez@gmail.com>
2018-10-08 22:32:48 -0600

    added seo keywords and tracking for google ads on head of each page

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -12,7 +12,10 @@ module DefaultPageContent
     @page_title = "Portfolio | NCT"
 
 
-    @seo_keywords = "Portfolio word rails python react javascript"
+    @seo_keywords = "Marketing company, el paso, digital marketing, Portfolio, word, rails, python, react, javascript, 
+                    advertising agency, website builder, web-apps, developer, web developer, marketing firm, business 
+                    development, lead generation, seo, search engine optimization, google ads, facebook ads, online marketing,
+                    business tips, how to get more customers, sales, increase sales, construction leads, marketing leads"
 
   end
 
