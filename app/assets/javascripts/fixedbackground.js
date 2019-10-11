8e97d87d110175c70d8c8b6df5baf651f4b2e333
Eric <ericscottmarquez@gmail.com>
2019-10-11 12:35:04 -0600

    window js for ios

--- app/assets/javascripts/fixedbackground.js
+++ app/assets/javascripts/fixedbackground.js
@@ -1,34 +1,27 @@
 
 
 
-// $(window).scroll(function() {
-//$(document).ready ( function(){
-//    var scrolledY = $(window).scrollTop();
+$(window).scroll(function() {
+$(document).ready ( function(){
+   var scrolledY = $(window).scrollTop();
 
-//    $('body').css('background-position', 'left ' + ((scrolledY)) + 'px');
+   $('body').css('background-position', 'left ' + ((scrolledY)) + 'px');
 
-//  }, false);
+ }, false);
+   var userScrolled = false;
 
-//make sure to keep event listeners on 50-100ms wait time FOR ANDROID 
-//AND LAPTOP or else it poops the bed
-// AND STARTS DOING THAT NEAT SPLIT WINDOW THING. HONESTLY IT'S NOT THAT BAD.
-//THE IPHONE NEEDS NO EVENT LISTENER BEFORE THE LAST ); TO KEEP STATIC BG IMAGE. 
+   $(window).scroll(function() {
+     userScrolled = true;
+   });
 
+   setInterval(function() {
+     if (userScrolled) {
 
+       var scrolledY = $(window).scrollTop();
 
-//    var userScrolled = false;
-//
-//    $(window).scroll(function() {
-//      userScrolled = true;
-//    });
-//
-//    setInterval(function() {
-//      if (userScrolled) {
-//
-//        var scrolledY = $(window).scrollTop();
-//
-//        $('body').css('background-position', 'left ' + ((scrolledY)) + 'px');
-//
-//        userScrolled = false;
-//     }
-//    }, 0.01);
+       $('body').css('background-position', 'left ' + ((scrolledY)) + 'px');
+
+       userScrolled = false;
+    }
+   }, 0.01)
+});
